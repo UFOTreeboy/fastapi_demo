82af7e0bcc9486771a3baff507f003635b425c7c
UFOTreeboy <jinn7431080@gmail.com>
2022-11-17 09:44:29 +0800

    2022_11_17

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fastapi_demo
+
+
+
+以該範本為主，來打造第一支FastAPI程式
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,Query
 from typing import Optional
 from pydantic import BaseModel
 
@@ -18,15 +18,15 @@ def get_item(item_id: int=Path(None,description="We can't find this id",gt=0)):
     return inventory[item_id]
 
 @app.get("/git-by-name")
-def get_item(name:Optional[str]=None):
+def get_item(name: str = Query(None, title="name", description="Name of item.", max_length=10, min_length=2)):
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-        return {"Data":"Not found"}
+    return {"Data":"Not found"}
 
 @app.post("/create-item")
 def create_item(item_id:int,item:Item):
     if item_id in inventory:
         return{"Erroe":"0123"}
-    inventory[item_id]={"name":item.name,"brand":item.brand,"price":item.price}
+    inventory[item_id]=item
     return inventory[item_id]
